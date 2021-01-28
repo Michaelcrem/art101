30b7044c4b97a4bb375605141b5c46d8b9fd0372
Michaelcrem <Michaelcrem@gmail.com>
2021-01-27 22:03:10 -0800

    commit

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,1 +1,27 @@
-// Print the board before each turn 
+//Print the board before each turn
+  //add board
+   // add 7 rows
+    // add css
+      // add two colors to board
+       // customize two colors
+
+// Accept the user input
+ //open DOM</pre>
+  //create a representation of the board
+
+// Validate user input
+ //register user response
+
+// Evaluate the possible moves
+ //limit moves
+  //create possible moves
+   // create limits on what is allowed per players turn
+
+// Choose computer moves
+  //reaction after player moves, or before player moves
+   //backwards once a piece reaches the other side of the board
+
+// Test for the win
+  //create variable that resets game
+    //create point system 
+     //add points 0-9
