b5ca5a0c3742c8c7b58f68c45bb2efd5d8e2946a
Michaelcrem <Michaelcrem@gmail.com>
2021-01-27 21:46:08 -0800

    commit to master

---
+++ Experiments/checkers/js/checkers.js
@@ -0,0 +1,29 @@
+// checkers.js
+// A cimple javascript implementation of checkers
+// author: Wes Modes & Art101 students
+// date: 2021
+//license: MIT 2.0
+
+
+
+
+
+//Put a drop of oil in pan
+//Crack the 2 eggs in a bowl
+//Mix the eggs until it reaches liquid consistancy
+//Pour eggs in heated pan
+//Stir the eggs until it reaches solid form
+//Turn off the stove burner
+//Put the eggs on your plate and enjoy
+
+
+
+
+//Print the board before each turn
+//
+//
+//Accept the user input
+//Validate user input
+//Evaluate the possible moves
+//Choose computer moves
+//Test the win
---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Matthew Palmer & Michael Cremonini
+ * Created: 27 January
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "Ford";
+ model = "Taurus";
+ color = "Rusty";
+ year = 1995;
+
+ // calculate
+ age = 2021 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
+console.log();
