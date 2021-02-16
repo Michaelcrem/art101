e8ad48caf5bdd674d7f828fcf42478d9355cac72
Michaelcrem <Michaelcrem@gmail.com>
2021-02-15 20:11:49 -0800

    push all

---
+++ lab10/js/lab.js
@@ -0,0 +1,29 @@
+// Lab 10- sort or scamble a name
+// Author:Michael Cremonini
+// Created: Febuary 15
+// License: Public Domain
+
+
+
+
+// find the button element
+buttonEl = document.getElementById("submit-button");
+console.log("button element:",buttonEl);
+
+// add an event listener to button
+buttonEl.addEventListener("click", function(){
+//find the name element
+nameEl = document.getElementById("name");
+//get value from the name element
+var userName = nameEl.value;
+//get the name element
+userName = userName.toLowerCase().split('').sort().reverse().join('');
+//this will shuffle 
+
+  console.log("user name:", userName)
+  //find the title element
+  titleEl = document.getElementById("title");
+  //change that element
+  titleEl.innerText = userName;
+
+});
--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -3,7 +3,7 @@
 //License: MIT (for everyone) copywritten by (for myself)
 
 
-//create a div element that has id="div_1" and text that says "this is annoying"
+//create a div element that has id="div_1" and text that says "Want know how many challenges we had"
 var new1El = document.getElementById("output");
 new1El.innerHTML = "Want know how many challenges we had?";
 
@@ -16,7 +16,7 @@ var new2El = document.createElement("button");
 document.body.appendChild(new2El); //keeps the button from disappearing
 new2El.innerHTML = "Challenges";
 
-//change the text from this annoying into "this is more annoying than I thought!" when Clicking the button
+//change the text from this annoying into "NONE!... ect." when Clicking the button
 new2El.onclick = function (){
         new1El.innerHTML = "NONE!... Just kidding, we did have some. We were having a hard time tyring to add in more buttons in our javascript since it was not connecting to our HTML. We found our error by looking over our code and fixing our new numbered variables. After that, we didn't have any challenges and it was straight forward. We both attended section the previous week which helped us a lot for this lab."
 
@@ -25,7 +25,7 @@ new2El.onclick = function (){
 
 
 
-//create a div element that has id="div_1" and text that says "this is annoying"
+//create a div element that has id="div_1" and text that says "Don't press the button"
 var new3El = document.getElementById("content");
 new3El.innerHTML = "Don't press the button";
 
@@ -38,14 +38,14 @@ var new4El = document.createElement("button");
 document.body.appendChild(new4El); //keeps the button from disappearing
 new4El.innerHTML = "Problems";
 
-//change the text from this annoying into "this is more annoying than I thought!" when Clicking the button
+//change the text from this annoying into "I got 99 Problems, but JavaScript isn't one.....etc." when Clicking the button
 new4El.onclick = function (){
         new3El.innerHTML = "I got 99 Problems, but JavaScript isn't one..... We are curious as to how to keep the buttons clickable without refreshing the page. We tried to exlpore this little problem in depth but we were happy with our initial result."
 
 }
 
 
-//create a div element that has id="div_1" and text that says "this is annoying"
+//create a div element that has id="div_1" and text that says "DONT PRESS THE THIRD BUTTON!"
 var new5El = document.getElementById("content2");
 new5El.innerHTML = "DONT PRESS THE THIRD BUTTON!";
 
@@ -55,11 +55,11 @@ console.log(new5El.innerHTML);
 
 //add a button
 var new6El = document.createElement("button");
-document.body.appendChild(new6El); //keeps the button from disappearing
+document.body.appendChild(new6El,); //keeps the button from disappearing
+
 new6El.innerHTML = "Results";
 
-//change the text from this annoying into "this is more annoying than I thought!" when Clicking the button
+//change the text from this annoying into "Here we have our results... ect." when Clicking the button
 new6El.onclick = function (){
         new5El.innerHTML = "Here we have our results. We made three new elements that allow us to click for more information. We created buttons in our javascript by using createElement and we connected our new elements in our HTML with our three different divs. By clicking on our buttons, you are able to see our information in our Results, Problems and Challenges Section!  ";
-
 }
