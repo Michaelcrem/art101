15531c3b8a194586b7b98aad230a5780cd15bd4b
Michaelcrem <Michaelcrem@gmail.com>
2021-02-18 10:43:38 -0800

    push to master

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -5,34 +5,25 @@
 
 
 
-$("#challenges").append("<button id=challenge-button>Press Me");
-//creates the button under challenges
+$("#challenges").append("<button id='c-button'>Button</button>");
+$("#problems").append("<button id='p-button'>Button</button>");
+$("#results").append("<button id='r-button'>Button</button>");
 
-$("#problems").append("<button id=problems-button>Press Me");
-//creates the button under problems
+// what the button do when clicked
+$("#c-button").click(function(){
 
-$("#results").append("<button id=results-button>Press Me");
-//creates the button under results
 
-$("#challenge-button").click(function(){
-  alert("Help! I'm trapped inside this computer!");
+  //another code for (this). parent
+//  $("#challenges").toggleClass("special")
+
+//traversing the DOM to the parent
+  $(this).parent().toggleClass("special");
 })
-//sends ann alert message after clicking on the challenge button
 
-$("#problems-button").click(function(){
-  alert("I can see you! Don't just sit there! Help me!");
+$("#p-button").click(function(){
+$(this).parent().toggleClass("special");
 })
-//sends ann alert message after clicking on the problems button
 
-$("#results-button").click(function(){
-  alert("Ok fine, don't do anything. I just stole all your info!!!");
+$("#r-button").click(function(){
+$(this).parent().toggleClass("special");
 })
-//sends ann alert message after clicking on the results button
-
-$(document).ready(function(){
-    $("p").on({
-        mouseenter: function(){
-            $(this).css("color", "green");
-        }
-    });
-});
