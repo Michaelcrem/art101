5d300dae74c6f6d4e2ba062f4795b08105bb580c
Michaelcrem <Michaelcrem@gmail.com>
2021-02-17 18:35:19 -0800

    commit all

---
+++ lab11/js/lab11.js
@@ -0,0 +1,30 @@
+// Lab 10- sort or scamble a name
+// Author:Michael Cremonini
+// Created: Febuary 17
+// License: Public Domain
+
+
+
+$("#challenges").append("<button id=challenge-button>Press Me");
+//creates the button under challenges
+
+$("#problems").append("<button id=problems-button>Press Me");
+//creates the button under problems
+
+$("#results").append("<button id=results-button>Press Me");
+//creates the button under results
+
+$("#challenge-button").click(function(){
+  alert("Help! I'm trapped inside this computer!");
+})
+//sends ann alert message after clicking on the challenge button
+
+$("#problems-button").click(function(){
+  alert("I can see you! Don't just sit there! Help me!");
+})
+//sends ann alert message after clicking on the problems button
+
+$("#results-button").click(function(){
+  alert("Ok fine, don't do anything. I just stole all your info!!!");
+})
+//sends ann alert message after clicking on the results button
---
+++ templated-snapshot/js/javascript.js
@@ -0,0 +1,4 @@
+function submitBio(){
+var name
+
+}
