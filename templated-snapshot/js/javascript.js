2b7a4166fa5270aa70d961fb9ac628d4580944ce
Michaelcrem <Michaelcrem@gmail.com>
2021-02-22 20:14:32 -0800

    push

--- templated-snapshot/js/javascript.js
+++ templated-snapshot/js/javascript.js
@@ -1,4 +1,20 @@
+//var navBar = document.getElementById("nav");
+//navBar.innerHTML= "this is the navigation bar";
 function submitBio(){
-var name
-
+var nameVar = document.getElementById("name").value;
+console.log(nameVar);
+var emailVar = document.getElementById("email").value;
+console.log(emailVar);
+var messageVar = document.getElementById("message").value;
+console.log(messageVar);
+document.getElementById('name_email_id').innerHTML ="Name: "+ nameVar+" Email: "+emailVar;
+document.getElementById('bio_id').innerHTML =messageVar;
 }
+document.getElementById("submit_bio_id").addEventListener('click',submitBio);
+
+// function colorMe(){
+//   //document.getElementById('name_email_id').style.color = "magenta";
+//   //document.getElementById('name_email_id').classList.add('color_me');
+//   document.getElementById('name_email_id').className += ' color_me';
+// }
+// document.getElementById("color_me_id").addEventListener('click',colorMe);
